bc1abd50bac5315c266d2188be6a8c0e22901d59
cnickels21 <cnickels_21@yahoo.com>
2020-05-18 15:23:31 -0500

    index view created

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +31,15 @@ namespace WebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
+
+            app.UseStaticFiles();
         }
     }
 }
